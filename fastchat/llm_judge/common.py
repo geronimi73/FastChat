2034122f994c520c7341da289e5468994c665e54
g588928812 <128976718+g588928812@users.noreply.github.com>
2023-08-08 18:52:07 +0200

    use TGW api instead of GPT4 for judge

--- fastchat/llm_judge/common.py
+++ fastchat/llm_judge/common.py
@@ -6,6 +6,8 @@ import ast
 import dataclasses
 import glob
 import json
+import requests
+
 import os
 import re
 import time
@@ -269,6 +271,11 @@ def run_judge_pair(question, answer_a, answer_b, judge, ref_answer, multi_turn=F
         judgment = chat_compeletion_anthropic(
             model, conv, temperature=0, max_tokens=1024
         )
+    elif model in ["text-generation-webui-api"]:
+        conv.set_system_message(system_prompt)
+        judgment = chat_compeletion_tgwApi(
+            model, conv, temperature=0, max_tokens=2048
+        )
     else:
         raise ValueError(f"Invalid judge model name: {model}")
 
@@ -396,6 +403,107 @@ def play_a_match_pair(match: MatchPair, output_file: str):
 
     return result
 
+def chat_compeletion_tgwApi(model, conv, temperature, max_tokens):
+    URI = f'http://127.0.0.1:5000/api/v1/chat'
+
+    for _ in range(API_MAX_RETRY):
+        try:
+            openai_messages = conv.to_openai_api_messages()
+            prompt_template="{sys}\n{msg}"
+            if len(openai_messages)==2:
+                prompt = prompt_template.format(
+                    sys=openai_messages[0]["content"],
+                    msg=openai_messages[1]["content"],
+                    )
+            else:
+                print(conv.messages)
+                input(f"len conv messages != !! {len(conv.messages)}")
+            # print("chat_compeletion_tgwApi")
+            # print(prompt)
+
+            request = {
+                'user_input': prompt,
+                'max_new_tokens': max_tokens,
+                'auto_max_new_tokens': False,
+                # 'history': [],
+                'mode': 'instruct',  # Valid options: 'chat', 'chat-instruct', 'instruct'
+                'character': 'Example',
+                'instruction_template': 'OrcaHashes',  # Will get autodetected if unset
+                # 'your_name': 'You',
+                # 'name1': 'name of user', # Optional
+                # 'name2': 'name of character', # Optional
+                # 'context': 'character context', # Optional
+                # 'greeting': 'greeting', # Optional
+                # 'name1_instruct': 'You', # Optional
+                # 'name2_instruct': 'Assistant', # Optional
+                # 'context_instruct': 'context_instruct', # Optional
+                # 'turn_template': 'turn_template', # Optional
+                'regenerate': False,
+                '_continue': False,
+                'stop_at_newline': False,
+                'chat_generation_attempts': 1,
+                # 'chat-instruct_command': 'Continue the chat dialogue below. Write a single reply for the character "<|character|>".\n\n<|prompt|>',
+
+                # Generation params. If 'preset' is set to different than 'None', the values
+                # in presets/preset-name.yaml are used instead of the individual numbers.
+                'preset': 'None',
+                'do_sample': True,
+                'temperature': 0.1,
+                'top_p': 0.1,
+                'typical_p': 1,
+                'epsilon_cutoff': 0,  # In units of 1e-4
+                'eta_cutoff': 0,  # In units of 1e-4
+                'tfs': 1,
+                'top_a': 0,
+                'repetition_penalty': 1.18,
+                'repetition_penalty_range': 0,
+                'top_k': 40,
+                'min_length': 0,
+                'no_repeat_ngram_size': 0,
+                'num_beams': 1,
+                'penalty_alpha': 0,
+                'length_penalty': 1,
+                'early_stopping': False,
+                'mirostat_mode': 0,
+                'mirostat_tau': 5,
+                'mirostat_eta': 0.1,
+
+                'seed': -1,
+                'add_bos_token': True,
+                'truncation_length': 2048,
+                'ban_eos_token': False,
+                'skip_special_tokens': True,
+                'stopping_strings': []
+            }
+
+            response = requests.post(URI, json=request)
+
+            if response.status_code == 200:
+                result = response.json()['results'][0]['history']
+            #     print(json.dumps(result, indent=4))
+            #     print()
+            #     print(result['visible'][-1][1])
+            # print(result)
+
+            output=result['visible'][-1][1]
+            # print("------------------------")
+            # print(output)
+            # print("------------------------")
+            # messages = conv.to_openai_api_messages()
+            # response = openai.ChatCompletion.create(
+            #     model=model,
+            #     messages=messages,
+            #     n=1,
+            #     temperature=temperature,
+            #     max_tokens=max_tokens,
+            # )
+            # output = response["choices"][0]["message"]["content"]
+            break
+        except Exception as e:
+            print("Exception in chat_compeletion_tgwApi:")
+            print(e)
+            time.sleep(API_RETRY_SLEEP)
+    return output
 
 def chat_compeletion_openai(model, conv, temperature, max_tokens):
     output = API_ERROR_OUTPUT
