2ae91da1f365ec74b349431c8285421b1bfed4db
g588928812 <128976718+g588928812@users.noreply.github.com>
2023-08-20 19:16:09 +0200

    Update compute_agreement.py
    add changes in https://colab.research.google.com/drive/1ctgygDRJhVGUJTQy8-bRZCl1WNcT8De6?usp=sharing

--- fastchat/llm_judge/compute_agreement.py
+++ fastchat/llm_judge/compute_agreement.py
@@ -11,7 +11,6 @@ import os
 
 import numpy as np
 
-
 def get_judge_name(judge):
     if isinstance(judge, list) and judge[0] == "gpt-4" and judge[1].startswith("pair"):
         return "gpt4-pair"
@@ -19,6 +18,7 @@ def get_judge_name(judge):
         return "human"
     if judge.startswith("author"):
         return "author"
+    return judge
 
 
 def revert(vote):
@@ -62,34 +62,26 @@ def equalvote(vote1, vote2):
         return True
     return vote1 == vote2
 
-
 # data: Dict[qid -> List[vote]]
 def get_mt_bench_agreement(data, judge1, judge2, ban):
-    if judge1.startswith("gpt4") and judge2 == "human":
+    if judge1 != "human" and judge2 == "human":
         stats = [0, 0]
         for votes in data.values():
-            if judge1 not in votes or judge2 not in votes:
-                continue
+            if judge1 not in votes or judge2 not in votes: continue
             assert len(votes[judge1]) == 1
-            if convertvote(votes[judge1][0]) in ban:
-                continue
+            if convertvote(votes[judge1][0]) in ban: continue
             for v in votes[judge2]:
-                if convertvote(v) in ban:
-                    continue
+                if convertvote(v) in ban: continue
                 stats[1] += 1
                 stats[0] += equalvote(votes[judge1][0], v)
         return stats[0], stats[1]
     elif judge1 == "human" and judge2 == "human":
         stats = [0, 0]
         for votes in data.values():
-            if "human" not in votes:
-                continue
+            if "human" not in votes: continue
             for i in range(len(votes["human"]) - 1):
                 for j in range(i + 1, len(votes["human"])):
-                    if (
-                        convertvote(votes["human"][i]) in ban
-                        or convertvote(votes["human"][j]) in ban
-                    ):
+                    if convertvote(votes["human"][i]) in ban or convertvote(votes["human"][j]) in ban:
                         continue
                     stats[1] += 1
                     stats[0] += equalvote(votes["human"][i], votes["human"][j])
@@ -102,29 +94,21 @@ def run_mt_bench_agreement(judges, votefiles):
     # votes[i]: List of votes
     votes = []
     for filename in votefiles:
-        with open(filename, "r") as f:
-            data = json.load(f)
-        votes.append(data)
+        data = []
+        for line in open(filename, "r"):
+            data.append(json.loads(line))
+        votes.append(data)        
 
     data = get_mt_bench_votes_data(votes)
 
     agree, total = get_mt_bench_agreement(data[0], judges[0], judges[1], ban=[])
-    print(
-        f"turn 1 with tie. #total: {total}, #agree: {agree}, ratio: {agree/total:.2f}"
-    )
+    print(f"turn 1 with tie. #total: {total}, #agree: {agree}, ratio: {agree/total:.2f}")
     agree, total = get_mt_bench_agreement(data[0], judges[0], judges[1], ban=["tie"])
-    print(
-        f"turn 1 without tie. #total: {total}, #agree: {agree}, ratio: {agree/total:.2f}"
-    )
+    print(f"turn 1 without tie. #total: {total}, #agree: {agree}, ratio: {agree/total:.2f}")
     agree, total = get_mt_bench_agreement(data[1], judges[0], judges[1], ban=[])
-    print(
-        f"turn 2 with tie. #total: {total}, #agree: {agree}, ratio: {agree/total:.2f}"
-    )
+    print(f"turn 2 with tie. #total: {total}, #agree: {agree}, ratio: {agree/total:.2f}")
     agree, total = get_mt_bench_agreement(data[1], judges[0], judges[1], ban=["tie"])
-    print(
-        f"turn 2 without tie. #total: {total}, #agree: {agree}, ratio: {agree/total:.2f}"
-    )
-
+    print(f"turn 2 without tie. #total: {total}, #agree: {agree}, ratio: {agree/total:.2f}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
