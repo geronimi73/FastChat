09fd475894332fb947c8429f9c6063d3305e6cf3
g588928812 <128976718+g588928812@users.noreply.github.com>
2023-08-22 01:28:25 +0200

    Update download_mt_bench_pregenerated.py
    default to the six models used

--- fastchat/llm_judge/download_mt_bench_pregenerated.py
+++ fastchat/llm_judge/download_mt_bench_pregenerated.py
@@ -3,7 +3,7 @@ Download the pre-generated model answers and judgments for MT-bench.
 """
 
 import sys
-sys.path.append("/home/g/ft-eval/FastChat")
+sys.path.append("/Users/geraldstampfel/Desktop/testbed/FastChat/")
 
 import os
 
@@ -14,18 +14,18 @@ filenames = [
     # "data/mt_bench/model_answer/baize-v2-13b.jsonl",
     # "data/mt_bench/model_answer/chatglm-6b.jsonl",
     # "data/mt_bench/model_answer/claude-instant-v1.jsonl",
-    # "data/mt_bench/model_answer/claude-v1.jsonl",
+    "data/mt_bench/model_answer/claude-v1.jsonl",
     # "data/mt_bench/model_answer/dolly-v2-12b.jsonl",
     # "data/mt_bench/model_answer/falcon-40b-instruct.jsonl",
     # "data/mt_bench/model_answer/fastchat-t5-3b.jsonl",
     "data/mt_bench/model_answer/gpt-3.5-turbo.jsonl",
-    # "data/mt_bench/model_answer/gpt-4.jsonl",
+    "data/mt_bench/model_answer/gpt-4.jsonl",
     # "data/mt_bench/model_answer/gpt4all-13b-snoozy.jsonl",
     # "data/mt_bench/model_answer/guanaco-33b.jsonl",
     # "data/mt_bench/model_answer/guanaco-65b.jsonl",
     # "data/mt_bench/model_answer/h2ogpt-oasst-open-llama-13b.jsonl",
     # "data/mt_bench/model_answer/koala-13b.jsonl",
-    # "data/mt_bench/model_answer/llama-13b.jsonl",
+    "data/mt_bench/model_answer/llama-13b.jsonl",
     # "data/mt_bench/model_answer/mpt-30b-chat.jsonl",
     # "data/mt_bench/model_answer/mpt-30b-instruct.jsonl",
     # "data/mt_bench/model_answer/mpt-7b-chat.jsonl",
@@ -36,7 +36,7 @@ filenames = [
     # "data/mt_bench/model_answer/rwkv-4-raven-14b.jsonl",
     # "data/mt_bench/model_answer/stablelm-tuned-alpha-7b.jsonl",
     # "data/mt_bench/model_answer/tulu-30b.jsonl",
-    # "data/mt_bench/model_answer/vicuna-13b-v1.3.jsonl",
+    # "data/mt_bench/model_answer/vicuna-13b-v1.2.jsonl",
     # "data/mt_bench/model_answer/vicuna-33b-v1.3.jsonl",
     # "data/mt_bench/model_answer/vicuna-7b-v1.3.jsonl",
     # "data/mt_bench/model_answer/wizardlm-13b.jsonl",
